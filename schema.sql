0bbbe865be383193ddbf5e12aaa8a251b7cb930d
mrcbq <marcobaquero@gmail.com>
2023-07-13 11:38:44 -0500

    Modify animals table

--- schema.sql
+++ schema.sql
@@ -24,3 +24,11 @@ CREATE TABLE species (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255)
 );
+
+-- Modify the animals table
+ALTER TABLE animals
+  DROP COLUMN species,
+  ADD COLUMN species_id INT,
+  ADD COLUMN owner_id INT,
+  ADD FOREIGN KEY (species_id) REFERENCES species(id),
+  ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
