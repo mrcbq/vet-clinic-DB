1e4c8bfff81b110b965df0b67d7c348ccd9e5d12
mrcbq <marcobaquero@gmail.com>
2023-07-13 12:04:56 -0500

    List of all animals that are pokemon (their type is Pokemon).

--- queries.sql
+++ queries.sql
@@ -78,3 +78,8 @@ SELECT a.name
 FROM animals a
 JOIN owners o ON a.owner_id = o.id
 WHERE o.full_name = 'Melody Pond';
+-- List of all animals that are pokemon (their type is Pokemon).
+SELECT a.name
+FROM animals a
+JOIN species s ON a.species_id = s.id
+WHERE s.name = 'Pokemon';
